af760b884fb55dee6c684ded118dfee5dac9d575
agus.adyandana <agus.adyandana@bigio.id>
2025-06-11 06:52:11 +0700

    Feat (response): Add base response

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,9 +1,12 @@
+/* eslint-disable @typescript-eslint/unbound-method */
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Response } from 'express';
 
 describe('AppController', () => {
   let appController: AppController;
+  let res: Response;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
@@ -12,11 +15,24 @@ describe('AppController', () => {
     }).compile();
 
     appController = app.get<AppController>(AppController);
+
+    res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    } as unknown as Response;
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  describe('index', () => {
+    it('should return service running successfully with base response', () => {
+      appController.getIndex(res);
+
+      expect(res.status).toHaveBeenCalledWith(200);
+
+      expect(res.json).toHaveBeenCalledWith(
+        expect.objectContaining({
+          status: true,
+        }),
+      );
     });
   });
 });
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { BaseResponse } from './utils/responses/base.dto';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getIndex(): BaseResponse<object> {
+    return new BaseResponse('Server running successfully');
   }
 }
